013aabd46e0b4b07aea26c37fcd84bf1184327c4
Violeta Malinova <violeta@mlnova.com>
2022-06-01 17:37:23 +0300

    Car card component

---
+++ src/components/pages/cars/CarCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../../utils/constants";
+
+const CarCard = ({ car, onClickDelete }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col gap-3 p-5 border border-teal-400 rounded-xl w-fit">
+      <div>Brand: {car.brand}</div>
+      <div>Model: {car.model}</div>
+      <div>Year: {car.buildYear}</div>
+      <div>Type: {car.vehicleType}</div>
+      <div>Fuel: {car.fuelType}</div>
+      <div>Seats: {car.numberOfSeats}</div>
+      <div>
+        <strong>Price per day: ${car.pricePerDay}</strong>
+      </div>
+      <button
+        className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-amber-600 hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500"
+        onClick={() => navigate(`${PATHS.Cars}/${car.id}`)}
+      >
+        Edit
+      </button>
+      <button
+        className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+        onClick={() => onClickDelete(car.id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default CarCard;
--- src/components/pages/cars/Cars.jsx
+++ src/components/pages/cars/Cars.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { deleteCar, getAllCars } from "../../../utils/http-utils/cars-requests";
 import { getIsAdmin } from "../../../utils/http-utils/user-requests";
 import { MainLayout } from "../../layout";
+import CarCard from "./CarCard";
 
 const Cars = () => {
   const isAdmin = getIsAdmin();
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    getAllCars().then((response) => setCars(response.data));
+  }, []);
+
+  const handleDelete = async (id) => {
+    await deleteCar(id);
+    getAllCars().then((response) => setCars(response.data));
+  };
+
   return (
     <MainLayout>
-      <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-row items-center justify-between mb-10">
         <h3 className="text-4xl font-bold w-fit">All cars</h3>
         {isAdmin && (
           <Link
@@ -17,6 +30,11 @@ const Cars = () => {
             Create
           </Link>
         )}
+      </div>
+      <div className="flex flex-row flex-wrap gap-6">
+        {cars.map((car) => (
+          <CarCard car={car} onClickDelete={handleDelete} />
+        ))}
       </div>
     </MainLayout>
   );
