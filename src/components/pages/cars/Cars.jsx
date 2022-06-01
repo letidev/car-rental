1f16f7ad0087191f7ccdc82d2c80b7d31f9fd7d2
Violeta Malinova <violeta@mlnova.com>
2022-06-01 09:09:48 +0300

    Adjust color

--- src/components/pages/cars/Cars.jsx
+++ src/components/pages/cars/Cars.jsx
@@ -9,7 +9,7 @@ const Cars = () => {
         <h3 className="text-4xl font-bold w-fit">All cars</h3>
         <Link
           to="/cars/create"
-          className="inline-block px-4 py-2 text-base font-medium text-white bg-teal-500 border border-transparent rounded-md hover:bg-opacity-75"
+          className="inline-block px-4 py-2 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-opacity-80"
         >
           Create
         </Link>
