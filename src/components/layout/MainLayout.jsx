99175e38491d242ce39b2705c2fbdbd440944923
Violeta Malinova <violeta@mlnova.com>
2022-06-02 10:13:08 +0300

    add pretty footer

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -17,8 +17,15 @@ const MainLayout = ({ children, className, role = "" }) => {
     <Guard>
       <main>
         <Header />
-        <div className="px-4 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
+        <div className="px-4 pb-10 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
           <div className={className}>{children}</div>
+        </div>
+        <div className="bg-teal-600">
+          <div className="px-4 py-10 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
+            <p className="text-white">
+              Imagine there are links to legal pages here
+            </p>
+          </div>
         </div>
       </main>
     </Guard>
