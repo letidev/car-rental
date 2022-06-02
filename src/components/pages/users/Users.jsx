b6b4b3a6e592c7f28d3091d1c845f53cab54ad16
Violeta Malinova <violeta@mlnova.com>
2022-06-02 09:08:27 +0300

    remove link

--- src/components/pages/users/Users.jsx
+++ src/components/pages/users/Users.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import {
   deleteUser,
   getAllUsersWithoutAdmin,
@@ -21,16 +20,9 @@ const Users = () => {
 
   return (
     <MainLayout role="admin">
-      <div className="flex flex-row items-center justify-between mb-10">
-        <h3 className="text-4xl font-bold w-fit">All users</h3>
-        <Link
-          to="/cars/create"
-          className="inline-block px-4 py-2 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md hover:bg-opacity-80"
-        >
-          Create
-        </Link>
-      </div>
-      <div className="flex flex-row flex-wrap gap-6">
+      <h3 className="text-4xl font-bold w-fit">All users</h3>
+
+      <div className="flex flex-row flex-wrap gap-6 mt-5">
         {users.map((user) => (
           <UserCard user={user} key={user.id} onClickDelete={handleDelete} />
         ))}
